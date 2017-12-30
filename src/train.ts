b28a07d5292c4fb4441cdcc4306b0bac9f9ae31d
Danny Blue <dannybluedesign@gmail.com>
2017-12-29 21:37:13 -0500

    trainer

--- src/train.ts
+++ src/train.ts
@@ -1,16 +1,36 @@
 import { Board, Team } from './board';
 import { MoveMaker } from './move-maker';
 
-const team: Team = Team.X;
+let board: Board;
+let engineX: MoveMaker;
+let engineO: MoveMaker;
 
-while (this.engineX.chooseWinner() === Team.E) {
-  switch (this.team) {
-    case Team.X:
-      this.engineX.makeMove(Team.X);
+for (let i = 0; i < 1000; i++) {
+  board = new Board();
+  engineX = new MoveMaker(board, Team.X);
+  engineO = new MoveMaker(board, Team.O);
 
-    case Team.O:
-      this.engineO.makeMove(Team.O);
-  }
+  train();
 }
 
-console.log(`Team: ${this.team} Wins!`);
+function train() {
+  let team: Team = Team.O;
+
+  while (engineX.chooseWinner() === Team.E) {
+    team = team === Team.O ? Team.X : Team.O;
+
+    switch (team) {
+      case Team.X:
+        engineX.makeMove();
+        break;
+
+      case Team.O:
+        engineO.makeMove();
+        break;
+    }
+
+    board.print();
+  }
+
+  console.log(`Team: ${engineX.chooseWinner()} Wins!`);
+}
