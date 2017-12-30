a1e059cb772452dd17a5eea613af610c253bab17
Cody Henthorne <codyh.zbi@gmail.com>
2017-12-29 23:01:13 -0500

    add player choice for playing

--- src/play.ts
+++ src/play.ts
@@ -2,12 +2,39 @@ import { Board, Team } from './board';
 import { MoveMaker } from './move-maker';
 
 export class PlayProgram {
-  gameEngine = new MoveMaker(this.board, Team.X);
+  aiTeam: Team;
+  userTeam: Team;
+  gameEngine: MoveMaker;
 
   constructor(public board: Board) {
-    this.gameEngine.makeMove();
+    if (process.argv.length === 2) {
+      process.stdout.write('Specify team, X or O\n');
+      process.exit();
+    }
 
+    let team = process.argv[2];
+    switch (team) {
+      case 'x':
+      case 'X':
+        this.userTeam = Team.X;
+        this.aiTeam = Team.O;
+        break;
+      case 'o':
+      case 'O':
+        this.userTeam = Team.O;
+        this.aiTeam = Team.X;
+        break;
+      default:
+        process.stdout.write('Specify team, X or O\n');
+        process.exit();
+    }
+
+    this.gameEngine = new MoveMaker(this.board, this.aiTeam);
+    if (this.aiTeam == Team.X) {
+      this.gameEngine.makeMove();
+    }
     this.displayState();
+    this.printPrompt();
 
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
@@ -19,9 +46,7 @@ export class PlayProgram {
 
     const winner = this.gameEngine.chooseWinner();
 
-    if (winner === Team.E) {
-      process.stdout.write(`Team ${Team.O}: `);
-    } else {
+    if (winner !== Team.E) {
       process.stdout.write(`Team ${winner} Wins! \n`);
 
       this.gameEngine.learnThings(winner);
@@ -32,6 +57,10 @@ export class PlayProgram {
     }
   }
 
+  printPrompt() {
+    process.stdout.write(`Team ${this.userTeam}: `);
+  }
+
   private onMoveReceived(txt: string) {
     const data = txt.split(',');
     const row = Number(data[0]);
@@ -40,17 +69,21 @@ export class PlayProgram {
     const space = this.board.squares[row * 3 + col];
 
     if (space === Team.E) {
-      this.board.set(row, col, Team.O);
+      this.board.set(row, col, this.userTeam);
 
       this.displayState();
 
       this.gameEngine.makeMove();
 
+      process.stdout.write(`Team ${this.aiTeam}'s move:`);
       this.displayState();
+
+      this.printPrompt();
     } else {
       process.stdout.write(
         `Space ${txt} is already taken by ${space}. Please try again \n`
       );
+      this.printPrompt();
     }
   }
 }
