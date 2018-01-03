9bb58d28fef6ce6f7b5cb31383807269bb0a9c6b
Danny Blue <dannybluedesign@gmail.com>
2018-01-02 22:51:04 -0500

    fix bad win condition

--- src/board.ts
+++ src/board.ts
@@ -44,20 +44,28 @@ export class Board {
     const squares = this.squares;
 
     if (this.checkBoard(0, 1, 2)) {
+      // row 1
       return squares[0];
     } else if (this.checkBoard(3, 4, 5)) {
+      // row 2
       return squares[3];
     } else if (this.checkBoard(6, 7, 8)) {
+      // row3
       return squares[6];
     } else if (this.checkBoard(0, 3, 6)) {
+      // col 1
       return squares[0];
     } else if (this.checkBoard(1, 4, 7)) {
+      // col 2
       return squares[1];
     } else if (this.checkBoard(2, 5, 8)) {
+      // col 3
       return squares[2];
     } else if (this.checkBoard(0, 4, 8)) {
+      // Diagonal top-left > bottom-right
       return squares[0];
-    } else if (this.checkBoard(2, 5, 6)) {
+    } else if (this.checkBoard(2, 4, 6)) {
+      // Diagonal top-right > bottom-left
       return squares[2];
     } else if (squares.every(square => square !== Team.Empty)) {
       return Team.CAT;
