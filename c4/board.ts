68a7e17fe5ec361ee6ad5f97ad1d7523893f97e1
Danny Blue <dannybluedesign@gmail.com>
2018-01-30 08:25:32 -0500

    remove unused Team enum

--- c4/board.ts
+++ c4/board.ts
@@ -1,12 +1,5 @@
 import { Board } from '@games/shared';
 
-export enum Team {
-  X = 'X',
-  B = 'O',
-  Empty = '.',
-  CAT = 'CAT' // Tie
-}
-
 export class C4Board extends Board {
   constructor() {
     super(6, 7);
